7099df9feababa072f01d6f574e36864e923b438
likhithchowdary26 <likhithchowdary26@gmail.com>
2022-03-31 11:32:25 +0530

    First Commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -126,7 +126,7 @@ MEDIA_ROOT = BASE_DIR
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shopifysolution965@gmail.com'
-EMAIL_HOST_PASSWORD = 'Eshop123+'
+EMAIL_PORT = 535
+EMAIL_HOST_USER = 'likhithchowdary562@gmail.com'
+EMAIL_HOST_PASSWORD = 'Likhith@26122001'
 EMAIL_USE_TLS = True
